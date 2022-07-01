05c7e5cd249242e2bc5327f6792cdf7fa3668d46
Shadiul Huda <shadiul.71@gmail.com>
2022-07-02 00:49:22 +0600

    🎨 Syntax Highlighter

--- pages/api/blog/[id].ts
+++ pages/api/blog/[id].ts
@@ -11,7 +11,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       date: faker.date.recent(),
       title: faker.lorem.sentence(),
       cover: faker.image.abstract(480, 480, true),
-      description: faker.lorem.paragraphs(10),
+      description: content,
     };
     setTimeout(() => {
       return res.status(200).json({ message: "Success", blog: blog });
@@ -20,3 +20,135 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(400).json({ message: "Not Found" });
   }
 }
+
+const content = `
+# Role based employees profile management system
+
+
+
+This project is mainly based on the role based permission of different routes.
+
+We have 4 different types of users:
+
+- Admin
+
+- System Manager
+
+- Business Manager
+
+- Employees
+
+
+
+Libraries used:
+
+- React with typescript
+
+- React router dom
+
+- Context API and react hooks
+
+- Nodemailer
+
+- React form hooks and yup resolver
+
+- Tailwind css and MUI
+
+
+
+Admin is the top of the pyramid which has a downline of System managers. Each system manager is registered with its admin id/username. The same structure follows for business managers and employees except employess is the lowest element of the pyramid so it wont have any downline.
+
+
+To prevent access form using non allowed routes we used conditional role based routes using  **react-router-dom** and **context api** .
+
+
+
+Route Objects:
+
+\`\`\`tsx {3-4, 8}
+export const UNPROTECTED_ROUTES: RouteObject[] = [
+  { path: "/", element: <RoleSelectionPage /> },
+  { path: PATHS_ADMIN.login, element: <AdminLoginPage /> },
+  { path: PATHS_SYSTEM_MANAGER.login, element: <SystemManagerLoginPage /> },
+  {
+    path: PATHS_SYSTEM_MANAGER.createAccount,
+    element: <SystemManagerCreateAccountPage />,
+  },
+  { path: "*", element: <div>Not Found</div> },
+];
+
+export const PROTECTED_ROUTES_ADMIN: RouteObject[] = [
+  {
+    path: "/admin",
+    children: [
+      { path: PATHS_ADMIN.login, element: <AdminLoginPage /> },
+      { path: PATHS_ADMIN.dashboard, element: <AdminDashboardPage /> },
+      {
+        path: PATHS_ADMIN.systemManagerInvitation,
+        element: <SystemManagerInvitationPage />,
+      },
+      { path: "*", element: <div>Not Found</div> },
+    ],
+  },
+];
+
+export const PROTECTED_ROUTES_SYSTEM_MANAGER: RouteObject[] = [
+  {
+    path: "/system-manager",
+    children: [
+      {
+        path: PATHS_SYSTEM_MANAGER.createAccount,
+        element: <SystemManagerCreateAccountPage />,
+      },
+      { path: PATHS_SYSTEM_MANAGER.login, element: <SystemManagerLoginPage /> },
+      {
+        path: PATHS_SYSTEM_MANAGER.dashboard,
+        element: <SystemManagerDashboardPage />,
+      },
+    ],
+  },
+];
+\`\`\`
+
+~~~tsx
+function App() {
+  const auth = useAuth();
+
+  let routes;
+
+  if (auth.isLoggedIn) {
+    switch (auth.role) {
+      case "admin":
+        routes = PROTECTED_ROUTES_ADMIN;
+        break;
+      case "system_manager":
+        routes = PROTECTED_ROUTES_SYSTEM_MANAGER;
+        break;
+      default:
+        routes = UNPROTECTED_ROUTES;
+        break;
+    }
+  } else {
+    routes = UNPROTECTED_ROUTES;
+  }
+
+  const renderRoutes = useRoutes(routes);
+  return <>{renderRoutes}</>;
+}
+~~~
+
+This will prevent user from accessing routes of different roles.
+
+
+
+For data visualization we can use the current role from the auth context then limit the user on viewing the data out of their role.
+
+
+
+
+
+#### Proposals for betterment
+
+- Admin should not have access to data that can be read and write by system manager. Admin's role should be limited to send invitaions of system managers and controlling the system managers permission and access. 
+
+`;
--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,7 +1,15 @@
-import { Box, Paper, Skeleton, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Paper,
+  Skeleton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import moment from "moment";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import ReactMarkdown from "react-markdown";
 import Layout from "../../components/Layout";
 import ResponsiveContainer from "../../components/Layout/ResponsiveContainer";
 import { useBlogQuery } from "../../queries/blogs/blog_query";
@@ -49,7 +57,9 @@ const BlogPage = (props: Props) => {
 
             <Box height={16} />
 
-            <Typography variant="body1" align="justify">
+            <BlogMarkdown markdown={data?.blog.description ?? ""} />
+
+            {/* <Typography variant="body1" align="justify">
               {isLoading && <Skeleton width="50%" />}
               {data &&
                 data.blog.description.split("\n").map((text, index) => (
@@ -57,7 +67,7 @@ const BlogPage = (props: Props) => {
                     {text}
                   </span>
                 ))}
-            </Typography>
+            </Typography> */}
           </Stack>
         </ResponsiveContainer>
       </Paper>
@@ -66,3 +76,77 @@ const BlogPage = (props: Props) => {
 };
 
 export default BlogPage;
+
+type BlogMarkdownProps = {
+  markdown: string;
+};
+
+import rangeParser from "parse-numeric-range";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
+import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import {
+  oneDark,
+  oneLight,
+} from "react-syntax-highlighter/dist/cjs/styles/prism";
+
+SyntaxHighlighter.registerLanguage("tsx", tsx);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("scss", scss);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("markdown", markdown);
+SyntaxHighlighter.registerLanguage("json", json);
+
+const BlogMarkdown = ({ markdown }: BlogMarkdownProps) => {
+  const theme = useTheme();
+  const syntaxTheme = theme.palette.mode === "dark" ? oneDark : oneLight;
+
+  const MarkdownComponents: object = {
+    code({ node, inline, className, ...props }: any) {
+      const match = /language-(\w+)/.exec(className || "");
+      const hasMeta = node?.data?.meta;
+
+      const applyHighlights: object = (applyHighlights: number) => {
+        if (hasMeta) {
+          const RE = /{([\d,-]+)}/;
+          const metadata = node.data.meta?.replace(/\s/g, "");
+          const strlineNumbers = RE?.test(metadata)
+            ? RE.exec(metadata)![1]
+            : "0";
+          const highlightLines = rangeParser(strlineNumbers);
+          const highlight = highlightLines;
+          const data: string | null = highlight.includes(applyHighlights)
+            ? "highlight"
+            : null;
+          return { data };
+        } else {
+          return {};
+        }
+      };
+
+      return match ? (
+        <SyntaxHighlighter
+          style={syntaxTheme}
+          language={match[1]}
+          PreTag="div"
+          className="codeStyle"
+          showLineNumbers={true}
+          wrapLines={hasMeta ? true : false}
+          useInlineStyles={true}
+          lineProps={applyHighlights}
+          {...props}
+        />
+      ) : (
+        <code className={className} {...props} />
+      );
+    },
+  };
+
+  return (
+    <ReactMarkdown components={MarkdownComponents}>{markdown}</ReactMarkdown>
+  );
+};
