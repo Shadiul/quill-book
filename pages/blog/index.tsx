651a78a34c2f848854e5f08d0b6ae3d395bdf113
Shadiul Huda <shadiul.71@gmail.com>
2022-07-02 07:24:12 +0600

    🎨 Update Blog Page

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,7 +1,23 @@
+import { Paper } from "@mui/material";
+import Layout from "../../components/Layout";
+import ResponsiveContainer from "../../components/Layout/ResponsiveContainer";
+import { useRecentBlogsQuery } from "../../queries/blogs/recent_blogs_query";
+import { useIsMobile, useIsTablet } from "../../utility/media_query_helper";
+
 type Props = {};
 
 const BlogsPage = (props: Props) => {
-  return <div>BlogsPage</div>;
+  const isMobile = useIsMobile();
+  const isTablet = useIsTablet();
+
+  const { isLoading, data } = useRecentBlogsQuery();
+  return (
+    <Layout title="Blog">
+      <Paper>
+        <ResponsiveContainer paddingY={isMobile ? 4 : 8}></ResponsiveContainer>
+      </Paper>
+    </Layout>
+  );
 };
 
 export default BlogsPage;
